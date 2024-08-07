dcb64e6e6d66481c489eda03fa95c2ed0e9c4df2
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2024-08-07 05:52:11 +0530

    mini project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React,{useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+
+function Profile() {
+
+    const {user} = useContext(UserContext)
+    if(!user || user.username=='') return <div>Please Login</div>
+
+  return (
+    <div>
+      Welcome {user.username}
+    </div>
+  )
+}
+
+export default Profile
